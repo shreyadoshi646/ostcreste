5c50024a33eac4f4214de26a918531825ab3f5b8
shreyadoshi646 <shreyadoshi646@gmail.com>
2026-03-16 15:10:01 +0530

    Add Git commands for initial repository setup

---
+++ github_tut/test.py
@@ -0,0 +1,12 @@
+ git init
+ git status
+ sudo git config --global user.name "your_GIT_username"
+ sudo git config --global user.email "your_GIT_emailID"
+ git remote add origin your_GIT_repo_URL
+ git remote set-url origin https://your_GIT_username:your_GIT_TOKEN@github.com/your_GIT_username/your_GIT_Repo_Name
+ sudo nano mycode.py
+ cat mycode.py 
+ git add .
+ git status
+ git commit -m "my first code"
+ git push origin master
